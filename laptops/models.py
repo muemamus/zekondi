5a53e13d535b31a997f355eb9d40b596dc772e26
muemamus <muemaaugustine9@gmail.com>
2020-07-18 14:15:16 +0000

    Add laptop model

---
+++ laptops/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Laptop(models.Model):
+
+    image = models.ImageField()
+    year = models.IntegerField()
+    screen_size = models.IntegerField()
+    hard_drive_size = models.CharField(max_length=20)
+    ram_size = models.CharField(max_length=20)
+    processor_speed = models.CharField(max_length=20)
+    delivery_option = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    instock = models.BooleanField()
+    stock_size = models.IntegerField()
+
+    def __str__(self):
+        return
