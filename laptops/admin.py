6c720193a157e7f6ce297cb36e573c08f21f4e57
muemamus <muemaaugustine9@gmail.com>
2020-07-20 03:58:18 +0000

    Customised laptops app admin

--- laptops/admin.py
+++ laptops/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Laptop
 
+class LaptopAdmin(admin.ModelAdmin):
+
+    list_display = (
+         "image",
+          "year",
+          "screen_size",
+          "hard_drive_size",
+          "ram_size",
+          "processor_speed",
+          "delivery_option",
+          "price",
+          "instock",
+          "stock_size"
+    )
 # Register your models here.
-admin.site.register(Laptop)
+admin.site.register(Laptop,LaptopAdmin)
 
