d2a56eee1661e77f4e68af47622ee6f3032667e4
muemamus <muemaaugustine9@gmail.com>
2020-07-20 07:18:12 +0000

    Added laptop details functionality

--- laptops/urls.py
+++ laptops/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_laptops, name='laptops')
+    path('', views.all_laptops, name='laptops'),
+    path('<laptop_id>', views.laptop_detail, name='laptop_detail'),
+
 ]
