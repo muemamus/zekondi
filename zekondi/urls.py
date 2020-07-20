6550340b3597eb3441a68cf42c296ca8c72b5af5
muemamus <muemaaugustine9@gmail.com>
2020-07-20 12:24:40 +0000

    Added shopping bag,urls and views

--- zekondi/urls.py
+++ zekondi/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('bag/', include('bag.urls')),
     path('laptops/', include('laptops.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
