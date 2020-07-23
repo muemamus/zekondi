9f377cd94cbb72eef90481162fab39906f280cdf
muemamus <muemaaugustine9@gmail.com>
2020-07-23 04:43:27 +0000

    Remove secret key and set debug

--- zekondi/settings.py
+++ zekondi/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+)a7(_z-zvpz89e%hn6z5cj1m(0xa2aod_y+fer6o-%7-o-0qh'
+SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMEMT' in os.environ
 
 ALLOWED_HOSTS = ['zekondi.herokuapp.com','localhost']
 
