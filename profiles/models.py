37169bfb609ed9d5b0eae16c2d00c5fe5fc149c4
muemamus <muemaaugustine9@gmail.com>
2020-07-25 22:27:38 +0000

    Push migrations files

--- profiles/models.py
+++ profiles/models.py
@@ -30,6 +30,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     
-    # if created:
-    UserProfile.objects.create(user=instance)
-    # instance.userprofile.save()
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
